02645956748d8480acca0292e510d6d8e260d461
zj <zj@webturing.com>
2018-08-02 22:43:41 +0800

    2018信息学夏令营《数论和组合数学》第一期普及班

--- 20180801PM/quickpower.cpp
+++ 20180801PM/quickpower.cpp
@@ -1,28 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-const int M=10;
-int power_poor(int a,int n) { //O(n)线性算法
-	int s=1;
-	for(int i=0; i<n; i++)
-		s=(s*a)%M;
-	return s;
+const int M = 10;
+int power_poor(int a, int n) {  // O(n)线性算法
+  int s = 1;
+  for (int i = 0; i < n; i++) s = (s * a) % M;
+  return s;
 }
-int power_quick(int a,int n) {//O(log(n))
-	a%=M;
-	if(n==0)	return 1;
-	if(n==1)	return a%M;
-	if(n%2==0) {
-		int b=a*a%M;
-		return power_quick(b,n/2);
-	} else {
-		int b=a*a%M;
-		return (power_quick(b,n/2)*a)%M;
-	}
+int power_quick(int a, int n) {  // O(log(n))
+  a %= M;
+  if (n == 0) return 1;
+  if (n == 1) return a;
+  if (n % 2 == 0) {
+    int b = a * a % M;
+    return power_quick(b, n / 2);
+  } else {
+    int b = a * a % M;
+    return (power_quick(b, n / 2) * a) % M;
+  }
 }
 int main() {
-	int a=3;
-	int n=200000000;
+  int a = 3;
+  int n = 200000000;
 
-//	cout<<power_poor(a,n)<<endl;
-		cout<<power_quick(a,n)<<endl;
+  //	cout<<power_poor(a,n)<<endl;
+  cout << power_quick(a, n) << endl;
 }
