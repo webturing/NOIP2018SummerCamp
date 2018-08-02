b962eaed0f3406e8b10a1f439d8036bc1f0650ef
zj <zj@webturing.com>
2018-08-03 05:47:06 +0800

    2018信息学夏令营《数论和组合数学》第一期普及班

--- 20180802AM/prime2.cpp
+++ 20180802AM/prime2.cpp
@@ -1,29 +1,30 @@
 #include<iostream>
 #include<cmath>
+
 using namespace std;
-bool primer(int n){//O(sqrt(n)/2)
-	if(n==2)
-		return 1;
-	if(n%2==0)
-		return 0;
-	int flag=1;
-	int m=sqrt(n);
-	for(int c=3;c<=m;c+=2){
-		if(n%c==0){
-			flag=0;
-			break;	
-		}
-	}
-	return flag;
+
+bool primer(int n) {//O(sqrt(n)/2)
+    if (n == 2)
+        return 1;
+    if (n % 2 == 0)
+        return 0;
+    int flag = 1;
+    int m = sqrt(n);
+    for (int c = 3; c <= m; c += 2) {
+        if (n % c == 0) {
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
 }
 
-int main()
-{
-	int n=1000000007;
-	
-	if(primer(n))
-		cout<<"It is a primer."<<endl;
-	else 
-		cout<<"It is a compositor"<<endl;
-	return 0;
+int main() {
+    int n = 1000000007;
+
+    if (primer(n))
+        cout << "It is a primer." << endl;
+    else
+        cout << "It is a compositor" << endl;
+    return 0;
 }
