d6589cec7543f3d9a1dec851f8de149e2df656c0
zj <zj@webturing.com>
2018-08-14 15:56:56 +0800

    并查集

--- 2018信息学夏令营(第二期提高班）/excices/并查集union-find.cpp
+++ 2018信息学夏令营(第二期提高班）/excices/并查集union-find.cpp
@@ -9,14 +9,17 @@ struct DisjoinSet {
     parent = new int[n];
     fill(parent, parent + n, -1);
   }
+  ~DisjoinSet() { delete[] parent; }
   friend ostream& operator<<(ostream& os, const DisjoinSet& ds) {
-    copy(ds.parent, ds.parent + ds._size, ostream_iterator<int>(os, " "));
+    for (int i = 0; i < ds._size; i++) os << i << "\t";
+    os << endl;
+    copy(ds.parent, ds.parent + ds._size, ostream_iterator<int>(os, "\t"));
     os << "Total=" << ds.size() << endl;
     return os;
   }
   inline int Find(int x) {
     if (parent[x] < 0) return x;
-    return parent[x] = Find(x);
+    return parent[x] = Find(parent[x]);
   }
   inline void Union(int root1, int root2) {
     if (root1 == root2) return;
@@ -34,10 +37,10 @@ struct DisjoinSet {
 };
 int main() {
   DisjoinSet ds;
-  for (int x = 0; x < 10; x++) {
+  for (int x = 0; x < 100; x++) {
     int i = rand() % ds._size;
     int j = rand() % ds._size;
-    ds.Union(i, j);
+    ds.Union(ds.Find(i), ds.Find(j));
     cout << i << " Union " << j << endl;
     cout << ds << endl;
   }
